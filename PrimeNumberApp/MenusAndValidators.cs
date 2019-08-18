d4e42f8463f045ebe8352feee893efc23efabf3b
Matt Hill <mlhill1@fullsail.edu>
2019-08-18 11:53:42 -0400

    C# Project

---
+++ PrimeNumberApp/MenusAndValidators.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MattsTools
+{
+    class Validator
+    {
+        public int GetInt()
+        {
+            int res;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out res))
+                {
+                    return res;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an integer value.");
+                }
+            }
+        }
+
+        public float GetFloat()
+        {
+            float res;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (float.TryParse(input, out res))
+                {
+                    return res;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a numeric value.");
+                }
+            }
+        }
+
+        public decimal GetDecimal()
+        {
+            decimal res;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (decimal.TryParse(input, out res))
+                {
+                    return res;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a decimal value.");
+                }
+            }
+        }
+
+        public string GetString()
+        {
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    return input;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a value.");
+                }
+            }
+        }
+    }
+
+    abstract class Menu
+    {
+        //Needed private properties
+        private string header { get; set; }
+
+
+
+
+        //Constructors
+        public Menu() { }
+        public Menu(string header)
+        {
+            Header = header;
+        }
+
+        //Getters and Setters
+        public string Header
+        {
+            get
+            {
+                return header;
+            }
+
+            set
+            {
+                header = value;
+            }
+        }
+
+
+
+
+
+    }
+
+    class WordMenu : Menu
+    {
+        public Dictionary<string, string> Options { get; set; }
+        public WordMenu(string header, Dictionary<string, string> options) :
+            base(header)
+        {
+            Options = options;
+        }
+
+        public string Display()
+        {
+
+            Console.WriteLine(Header);
+            List<string> keys = new List<string>();
+            foreach (var option in Options)
+            {
+                Console.WriteLine(option.Key + ": \t" + option.Value);
+                keys.Add(option.Key.ToLower());
+            }
+            Console.WriteLine("\r\n");
+            while (true)
+            {
+                Validator validator = new Validator();
+                string res = validator.GetString().ToLower();
+                if (keys.Contains(res))
+                {
+                    return res;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter one of the above menu options.");
+                }
+            }// End of the menu loop
+
+        }
+
+
+    }
+
+    class NumberMenu : Menu
+    {
+        public List<string> Options { get; set; }
+        public NumberMenu(string header, List<string> options) :
+            base(header)
+        {
+            Options = options;
+        }
+
+        public int Display()
+        {
+
+            Console.WriteLine(Header);
+            int i = 1;
+            foreach (var option in Options)
+            {
+                Console.WriteLine(i + ": " + option);
+                i++;
+            }
+            Console.WriteLine("\r\n");
+            while (true)
+            {
+                Validator validator = new Validator();
+                int res = validator.GetInt();
+                //makes sure the number picked is a number on the list
+                if (res <= i - 1 && res > 0)
+                {
+                    return res;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a whole number between 1 and {0}.", i - 1);
+                }
+            }// End of the menu loop
+
+        }
+
+        public string DisplayReturnsString()
+        {
+
+
+            Console.WriteLine(Header);
+            int i = 1;
+            foreach (var option in Options)
+            {
+                Console.WriteLine(i + ": " + option);
+                i++;
+            }
+            Console.WriteLine("\r\n");
+            while (true)
+            {
+                Validator validator = new Validator();
+                int res = validator.GetInt();
+                //makes sure the number picked is a number on the list
+                if (res <= i - 1 && res > 0)
+                {
+                    return Options[res - 1];
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a whole number between 1 and {0}.", i - 1);
+                }
+            }// End of the menu loop
+
+        }
+
+
+    }
+
+
+    class FancyMenu : Menu
+    {
+        public Dictionary<string, string> Options { get; set; }
+        public FancyMenu(string header, Dictionary<string, string> options) :
+            base(header)
+        {
+            Options = options;
+        }
+
+        public string Display()
+        {
+
+
+            Console.WriteLine(Header);
+            List<string> keys = new List<string>();
+            int menuNumber = 0;
+            foreach (var option in Options)
+            {
+                menuNumber++;
+                Console.WriteLine(menuNumber + " - " + option.Key + ": " + option.Value);
+                keys.Add(option.Key.ToLower());
+            }
+            Console.WriteLine("\r\n");
+            while (true)
+            {
+                Validator validator = new Validator();
+
+                string res = validator.GetString().ToLower();
+                int resNum;
+
+                if (int.TryParse(res, out resNum))
+                {
+                    //logic for menu options with numbers
+                    if (resNum > 0 && resNum <= keys.Count)
+                    {
+                        return keys[resNum - 1];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid number or type a valid menu option.");
+                    }
+                }
+                else if (keys.Contains(res))
+                {
+                    return res;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number or type a valid menu option.");
+                }
+
+            }// End of the menu loop
+
+        }
+
+
+    }
+
+}
---
+++ PrimeNumberApp/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using MattsTools;
+
+namespace MatthewHill_CE08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+            while (choice != 2
+                )
+            {
+
+
+
+                string fileMenuHeader = "Would you like to check if a number is prime?";
+                List<string> fileOptions = new List<string>();
+                fileOptions.Add("Yes");
+                fileOptions.Add("No");
+                //This is a custom class for setting up menus in the console. It takes a <String> header and a <List> of options. See the MenusAndValidators.cs file for further explanation.
+                NumberMenu mainMenu = new NumberMenu(fileMenuHeader, fileOptions);
+                choice = mainMenu.Display();
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Clear();
+                        //Calls the CheckIfPrime Function if the user wants to do that.
+                        Console.WriteLine(CheckIfPrime());
+                        break;
+                        //Will Cause the program to close.
+                    case 2:
+
+                        break;
+
+                    default:
+                        break;
+                }
+                
+            }
+            
+
+        }
+        
+        //The meat of the prime checker.
+        private static string CheckIfPrime()
+        {
+            //Custom class used for input validation see the MenusAndValidators.cs file for further explanation.
+            Validator validator = new Validator();
+
+            //Instantiate the number we are going to check.
+            int isItPrime = 0;
+            //A loop to make sure the number we are checking for is over zero.
+            while(isItPrime < 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter a positive whole number now.");
+                isItPrime = validator.GetInt();
+                Console.WriteLine("Processing...");
+            }
+
+            //testNumber will effectively be the i-- that we use as our divisor.
+            int testNumber = isItPrime;
+            //This List will store any testNumber that divides equally into the isItPrime number.
+            List<int> divisors = new List<int>();
+
+            //This loop checks for a modulus == 0 result and then stores that number into the divisors list
+            while (testNumber > 1)
+            {
+                //Used these lines during testing. Left them in so you can see the thought process.
+                //Console.WriteLine(testNumber);
+                int remain = isItPrime % testNumber;
+                //Used these lines during testing. Left them in so you can see the thought process.
+                //Console.WriteLine(remain);
+                //Console.WriteLine();
+                if (remain == 0)
+                {
+                    divisors.Add(testNumber);
+                }
+                testNumber--;
+            }
+            //Used these lines during testing. Left them in so you can see the thought process.
+            //Console.WriteLine("Divisors:");
+            //divisors.ForEach(i => Console.Write(i+" "));
+            //Console.WriteLine();
+
+
+            Console.Clear();
+            //If the divisors list is greater than 1 then there is more than one divisor greater than 1 and the number is not prime.
+            if (divisors.Count > 1)
+            {
+                return FindPrimeFactors(isItPrime);
+                
+            }
+            //Otherwise, it is prime.
+            else
+            {
+                return isItPrime + " is prime.";
+            }
+        }
+        //This is the function that finds prime factors for non prime numbers.
+        private static string FindPrimeFactors(int notPrime)
+        {
+            //Need a list to store prime factors
+            List<int> divisbleBy = new List<int>();
+            //instantiate numbers we are gonna use for our math functions
+            int maxNumber = notPrime;
+            int testnumber = 1;
+            //Loop to keep running the math.
+            while(testnumber < maxNumber)
+            {
+                //Starting with adding to the test number. We can start at 2 since we already know the number is not prime.
+                testnumber++;
+                //Used these lines during testing. Left them in so you can see the thought process.
+                //Console.WriteLine(testnumber);
+                //Checking to see if the testnumber divides equally into the maxNumber
+                if (maxNumber%testnumber == 0)
+                {
+                    //Making sure we don't add any extra prime factors
+                    if (divisbleBy.Contains(testnumber))
+                    { }
+                    else
+                    {
+                        divisbleBy.Add(testnumber);
+                    }
+                    //Reducing the maxNumber and resetting the testnumber before we start over
+                    maxNumber = maxNumber / testnumber;
+                    testnumber = 1;
+                }
+                
+            }
+            Console.Write(notPrime + "'s prime factors are:");
+            divisbleBy.ForEach(i => Console.Write(i + " "));
+            Console.WriteLine();
+         
+            return notPrime + " is not prime.";
+        }
+        }// end of class program
+
+
+}// end of namespace
